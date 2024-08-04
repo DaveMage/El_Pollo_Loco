112ce23bad05680bba9765df4736dd7d3bbe704b
DaveMage <152330450+DaveMage@users.noreply.github.com>
2024-08-04 19:09:47 +0200

    add level folder

---
+++ js/levels/level1.js
@@ -0,0 +1,25 @@
+const level1 = new Level(
+    [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ],[
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+    ],[
+        new BackgroundObject('img/5_background/layers/air.png', 0, 0),                      // air
+        new BackgroundObject('img/5_background/layers/air.png', 719, 0),
+        new BackgroundObject('img/5_background/layers/air.png', 1438, 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 0),          // 3te schicht rosa
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719, 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 1438, 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0, 0),         //dahinter rot
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719, 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 1438, 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 0),          // ganz vorne kaktus
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719, 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 1438, 0),
+    ]
+);
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -1,30 +1,8 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-    ];
-    clouds = [
-        new Cloud(),
-        new Cloud(),
-        new Cloud(),
-        new Cloud(),
-    ];
-    backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/air.png', 0, 0),                      // air
-        new BackgroundObject('img/5_background/layers/air.png', 719, 0),
-        new BackgroundObject('img/5_background/layers/air.png', 1438, 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 0),          // 3te schicht rosa
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719, 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 1438, 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0, 0),         //dahinter rot
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719, 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 1438, 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 0),          // ganz vorne kaktus
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719, 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 1438, 0),
-    ];
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObjects = level1.backgroundObjects;
     canvas;
     ctx;
     keyboard;
