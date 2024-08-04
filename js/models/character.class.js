b974eb89d740575874a13ccecf9f42564e800a46
DaveMage <152330450+DaveMage@users.noreply.github.com>
2024-08-04 16:49:18 +0200

    first moving animations

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,8 +1,29 @@
 class Character extends MovableObject {
-
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png',
+    ];
 
     constructor() {
         super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    animate() {
+
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;  // let i = 0 % 6 - 
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
+
     }
 
     jump() {
@@ -10,3 +31,11 @@ class Character extends MovableObject {
     }
 
 }
+
+
+// let i = this.currentImage % this.IMAGES_WALKING.length;   Modulu besagt  let i = 0 % 6
+// wenn i = 1 dann 1 % 6 . simpel reicht die 1 nicht um die 6 zu füllen mann bekommt den rest dann noch als zahl dabei. 
+// 1 % 6 = 0, rest 1
+// 2 % 6 = 0, rest 2
+// etc.
+// 6 % 6 = 1, rest 0
--- js/models/cloud.class.js
+++ js/models/cloud.class.js
@@ -3,9 +3,16 @@ class Cloud extends MovableObject {
     height = 250;
     width = 300;
 
+
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png')
-
         this.x = Math.random() * 650;
+        this.animate();
     }
+
+    animate() {
+        this.moveLeft();
+    }
+
+
 }
