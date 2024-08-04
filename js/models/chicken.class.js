dc50f5ba5c1c46242bf323e883e606ed80c5004b
DaveMage <152330450+DaveMage@users.noreply.github.com>
2024-08-04 14:45:52 +0200

    assets eingepflegt

--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -1,8 +1,10 @@
 class Chicken extends MovableObject {
-
-
+    y = 325;
+    height = 100; 
     constructor() {
-        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/2_w.png');
+        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+
+        this.x = 200 + Math.random() * 500;
     }
 
 }
---
+++ js/models/cloud.class.js
@@ -0,0 +1,11 @@
+class Cloud extends MovableObject {
+    y = 50;
+    height = 250;
+    width = 300;
+
+    constructor() {
+        super().loadImage('img/5_background/layers/4_clouds/1.png')
+
+        this.x = Math.random() * 650;
+    }
+}
